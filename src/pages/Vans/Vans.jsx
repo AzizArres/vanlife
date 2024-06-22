6e162fca28db9b14acbe54148bc999006d23d32b
Aziz <63376723+AzizArres@users.noreply.github.com>
2024-06-22 12:06:44 +0100

    vans filters update

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,16 +1,24 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
+
 
 
 export default function Vans(){
+    const [searchParams, setSearchParams] = useSearchParams()
     const [vans, setVans] = React.useState([])
+
+    const typeFilter = searchParams.get("type")
+
     React.useEffect(()=>{
         fetch("/api/vans")
         .then(res => res.json())
         .then(data => setVans(data.vans))
     }, [])
-    const vanElements = vans.map(van => (
+
+    const filteredVans = typeFilter ? vans.filter(van => van.type === typeFilter) : vans
+
+    const vanElements = filteredVans.map(van => (
         <div key={van.id} className="van">
             <Link to={`/vans/${van.id}`}>
                 <img src={van.imageUrl} alt="" />
@@ -22,10 +30,17 @@ export default function Vans(){
             </Link>
         </div>
     ))
+
     return(
         <div className="vans">
             <h1>Explore our van options</h1>
             <div className="van-list-container">
+                <nav className="filters">
+                    <button onClick={()=> setSearchParams("?type=simple")} className="van-type-button simple">Simple</button>
+                    <button onClick={()=> setSearchParams("?type=luxury")} className="van-type-button luxury">Luxury</button>
+                    <button onClick={()=> setSearchParams("?type=rugged")} className="van-type-button rugged">Rugged</button>
+                    <button onClick={()=> setSearchParams("")} className="van-type-button clear-filter">Clear filter</button>
+                </nav>
                 <div className="van-list">
                     {vanElements}
                 </div>
